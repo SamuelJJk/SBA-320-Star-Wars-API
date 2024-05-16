7f9b36ae4f4ecb0d4ef12677ce197c09914fc5d9
SamuelJJk <samueljean.j24@gmail.com>
2024-05-16 14:06:19 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const getStarWarsData = async()=>{
+    const response = await fetch('https://swapi.dev/api');
+    const data = await response.json();
+    console.log(data)
+  }
+  getStarWarsData();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
